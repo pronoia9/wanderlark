901ac38a4dbbb01978f21b0265eeac7e6144c082
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-05 17:13:36 +0300

    rename wanderlark

--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,7 @@
 // NAVIGATION
 export const NAV_LINKS = [
   { href: '/', key: 'home', label: 'Home' },
-  { href: '/', key: 'how_wanderlark_work', label: 'How Hilink Work?' },
+  { href: '/', key: 'how_wanderlark_work', label: 'How WanderLark Work?' },
   { href: '/', key: 'services', label: 'Services' },
   { href: '/', key: 'pricing ', label: 'Pricing ' },
   { href: '/', key: 'contact_us', label: 'Contact Us' },
@@ -24,7 +24,7 @@ export const FEATURES = [
     icon: '/calendar.svg',
     variant: 'green',
     description:
-      "Schedule an adventure with friends. On holidays, there are many interesting offers from Hilink. That way, there's no more discussion",
+      "Schedule an adventure with friends. On holidays, there are many interesting offers from WanderLark. That way, there's no more discussion",
   },
   {
     title: 'Technology using augment reality',
@@ -46,11 +46,11 @@ export const FEATURES = [
 export const FOOTER_LINKS = [
   {
     title: 'Learn More',
-    links: ['About Hilink', 'Press Releases', 'Environment', 'Jobs', 'Privacy Policy', 'Contact Us'],
+    links: ['About WanderLark', 'Press Releases', 'Environment', 'Jobs', 'Privacy Policy', 'Contact Us'],
   },
   {
     title: 'Our Community',
-    links: ['Climbing xixixi', 'Hiking wanderlark', 'Hilink kinthill'],
+    links: ['Climbing xixixi', 'Hiking wanderlark', 'WanderLark kinthill'],
   },
 ];
 
