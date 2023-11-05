ab05ffa3422a5d9606db58587adc07eb65efb2cf
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-05 17:10:00 +0300

    add padding to links only for small devices

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 
       <ul className='hidden h-full gap-12 lg:flex'>
         {NAV_LINKS.map((link) => (
-          <Link href={link.href} key={link.key} className='regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold'>
+          <Link href={link.href} key={link.key} className='regular-16 text-gray-50 flexCenter cursor-pointer s:pb-1.5 transition-all hover:font-bold'>
             {link.label}
           </Link>
         ))}
