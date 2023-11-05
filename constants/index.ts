0fcc972935ae2e9c1affe0498f4e6bd14e282de1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-05 17:15:12 +0300

    fix typo

--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,7 @@
 // NAVIGATION
 export const NAV_LINKS = [
   { href: '/', key: 'home', label: 'Home' },
-  { href: '/', key: 'how_wanderlark_work', label: 'How WanderLark Work?' },
+  { href: '/', key: 'how_wanderlark_work', label: 'How WanderLark Works?' },
   { href: '/', key: 'services', label: 'Services' },
   { href: '/', key: 'pricing ', label: 'Pricing ' },
   { href: '/', key: 'contact_us', label: 'Contact Us' },
