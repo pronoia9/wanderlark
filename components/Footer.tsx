008ef1ce1dd24bdacd440879d640f3ee338276ff
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-11-05 17:14:19 +0300

    format

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,22 @@
-import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from '@/constants';
+import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from '@/constants';
+
+type FooterColumnProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const FooterColumn = ({ title, children }: FooterColumnProps) => {
+  return (
+    <div className='flex flex-col gap-5'>
+      <h4 className='bold-18 whitespace-nowrap'>{title}</h4>
+      {children}
+    </div>
+  );
+};
 
 const Footer = () => {
   return (
@@ -54,20 +69,6 @@ const Footer = () => {
         <p className='regular-14 w-full text-center text-gray-30'>2023 WanderLark | All rights reserved</p>
       </div>
     </footer>
-  );
-};
-
-type FooterColumnProps = {
-  title: string;
-  children: React.ReactNode;
-};
-
-const FooterColumn = ({ title, children }: FooterColumnProps) => {
-  return (
-    <div className='flex flex-col gap-5'>
-      <h4 className='bold-18 whitespace-nowrap'>{title}</h4>
-      {children}
-    </div>
   );
 };
 
